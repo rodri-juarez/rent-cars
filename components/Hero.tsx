ade02cf3f72d26979dbdd9e05c62a48da754daf7
Rodrigo Juarez <juarezr100@gmail.com>
2023-11-12 23:22:32 -0300

    feat: add CustomButton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,6 +17,11 @@ const Hero = () => {
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking process
         </p>
+        <CustomButton
+          title={"Explore Cars"}
+          containerStyles={`bg-primary-blue text-white rounded-full mt-10`}
+          handleClick={handleScroll}
+        />
       </div>
     </div>
   )
