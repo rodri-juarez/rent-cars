f02075128795a69df851d6fba2358b14054a7538
Rodrigo Juarez <juarezr100@gmail.com>
2023-11-14 16:00:54 -0300

    style: add img bg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,12 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="" alt="Hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
+        </div>
+      </div>
     </div>
   )
 }
