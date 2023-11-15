81ceaab4a123d1d1eebddb004c5d85c09d18431b
Rodrigo Juarez <juarezr100@gmail.com>
2023-11-15 13:15:14 -0300

    feat: add fetch helper

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,19 @@
-import Hero from "./Hero";
-import CustomButton from './CustomButton';
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import SearchBar from "./SearchBar";
-import CustomFilter from "./CustomFilter";
-import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import Hero from "./Hero";
 
 export {
+  Hero,
   CarCard,
   CustomButton,
-  Footer,
-  Hero,
-  Navbar,
-  SearchBar,
   CustomFilter,
-  SearchManufacturer
-}
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import {FilterProps} from "@/app/page";
 import {CarProps} from "@/components/CarCard";
 
 export const updateSearchParams = (type: string, value: string) => {
@@ -37,4 +38,40 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append('angle', `${angle}`);
 
   return `${url}`;
+}
+
+export const deleteSearchParams = (type: string) => {
+  // Set the specified search parameter to the given value
+  const newSearchParams = new URLSearchParams(window.location.search);
+
+  // Delete the specified search parameter
+  newSearchParams.delete(type.toLocaleLowerCase());
+
+  // Construct the updated URL pathname with the deleted search parameter
+  const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
+
+  return newPathname;
+};
+
+export async function fetchCars(filters: FilterProps) {
+  const {manufacturer, year, model, limit, fuel} = filters;
+
+  // Set the required headers for the API request
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  // Set the required headers for the API request
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
+
+  // Parse the response as JSON
+  const result = await response.json();
+
+  return result;
 }
